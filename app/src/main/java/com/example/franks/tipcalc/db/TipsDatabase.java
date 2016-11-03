08c9fd37afd1409fb7b98e8bdda817c86e97964d
Franks <r.g.francisco40@gmail.com>
2016-11-03 08:52:42 -0600

    add Database ORM Especification

---
+++ app/src/main/java/com/example/franks/tipcalc/db/TipsDatabase.java
@@ -0,0 +1,13 @@
+package com.example.franks.tipcalc.db;
+
+/**
+ * Created by franks on 3/11/16.
+ */
+
+@Database(name = TipsDatabase.NAME, version = TipsDatabase.VERSION)
+
+public class TipsDatabase {
+    public static final String NAME = "Tips";
+    public static final int VERSION = 1;
+
+}
